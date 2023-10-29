dcfff9c950dbde73411cc25e2676ca7b0a9a7fb5
shaheerkhan111 <149266345+shaheerkhan111@users.noreply.github.com>
2023-10-29 14:01:38 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,14 @@
+// import inquirer from "inquirer";
+// import chalk from "chalk";
+
+
+// let answers = await inquirer.prompt([
+//     {
+//         name: "Sentence",
+//         type: "input",
+//         message: "Enter your sentence to count the word: "
+//     }
+// ])
+
+// let words = await answers.sentence.trim( ).split("")
+// console.log(chalk.red.bold.italic(`Your sentence word count is ${chalk.yellow.bold.italic(words.length)}`))
